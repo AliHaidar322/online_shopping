4266fd35ffc4382e47793a3a5e144a5db5d79037
alihaidar322 <alibinkhalid322@gmail.com>
2024-04-25 18:36:48 -0400

    Configurations done for Item and categories

--- item/models.py
+++ item/models.py
@@ -15,7 +15,14 @@ class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
     price = models.FloatField()
     is_sold = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering=('name',)
+
+    def __str__(self):
+        return self.name
